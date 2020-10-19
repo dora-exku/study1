c8878ac490525ff89eef892c01b66f311b46ab9a
vagrant <vagrant@homestead>
2020-10-19 14:12:56 +0000

    更新用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -35,4 +35,23 @@ class UsersController extends Controller
         session()->flash('success', '欢迎您，新用户');
         return redirect()->route('users.show',[$user]);
     }
+
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'required|confirmed|min:5'
+        ]);
+        $user->update([
+            'name' => $request->name,
+            'password' => bcrypt($request->password)
+        ]);
+        session()->flash('success', '用户资料更新成功');
+        return redirect()->route('users.show', $user->id);
+    }
 }
